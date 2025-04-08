58f3c6d325dbac93b06b1a1d3693cdf9b5426096
zaveriadi-cp <aditya.zaveri@classplus.co>
2025-04-08 16:21:53 +0530

    initalized git

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutPage() {
+    console.log('About page loaded')
+  return (
+    <div>
+      <h1>hello about page</h1>
+    </div>
+  )
+}
+
+export default AboutPage
